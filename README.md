97dfd5249f68e4e7b8775634ad869aa9318a0f9d
John <TPgMNMKQLre897@rs.ru>
2024-02-17 11:42:37 +0300

    Test code

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Attiny13-Generator
-Square signal generator on microcontroller ATtiny13. Language C.
+Square signal generator base on ATtiny13 avr chip.
+Firmware written on C in IDE Microchip Studio 7.02594.
+Using two button for change output frequency.
+For tests speaker connect to signal pin.
+
+If you change or add interrupts in code. See it name on header file
+Program Files\Microchip\xc8\v2.36\dfp\xc8\avr\include\avr\iotn13a.h
--- Sources/Attiny13Generator/main.c
+++ Sources/Attiny13Generator/main.c
@@ -1,38 +1,43 @@
-/*
- * Attiny13Generator.c
- *
- * Created: 17.02.2024 8:59:47
- * Author : Team
- */ 
-#define F_CPU 4800000UL
+//#define F_CPU 4800000UL
+
+#define START_PULSE_LENGTH 200; //Default on mcu start.
+#define MIN_PULSE_LENGTH 2
+#define MAX_PULSE_LENGTH 400000 //2Hz
+#define PULSE_LENGTH_STEP 20
+
 #include <Util/delay.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-uint8_t timeCounter1 = 0; //Значение программного счетчика.
-uint32_t pulseLength = 0; //Заданная длина импульса.
-uint32_t pulseLengthCounter = 0; //Счетчик длины импульса.
-uint8_t pulseLevel = 0; //Уровень сигнала на выходе 1/0.
+uint8_t timeCounter1 = 0; //Software counter for button.
+uint32_t pulseLength = 0; //Setup pulse length.
+uint32_t pulseLengthCounter = 0; //Pulse length counter.
+uint8_t pulseLevel = 0; //Level on output 1/0.
 
 //Timer/Counter0 Overflow
-ISR(TIMER0_OVF_vect){
-	
-	//if(timeCounter1 > 5)
-	//{
-		//timeCounter1 --;
-	//}
-	//else
-	//{
-		//timeCounter1 = 0;
-		//uint8_t btnDown = PINB && 0x04;
-		//if(btnDown != 0x04) //Подключен к земле.
-		//{
-			////if(pulseLength)
-		//}	
-	//}
+ISR(TIM0_OVF_vect){
+
+	//Software counter.
+	timeCounter1 ++;
+	if(timeCounter1 >= 5)
+	{
+		timeCounter1 = 0; //Clear software counter.
+		
+			uint8_t btnDown = PINB & 0b00010000;
+			if(btnDown == 0 & pulseLength <= MAX_PULSE_LENGTH) //Push this button.
+			{
+				pulseLength+= PULSE_LENGTH_STEP;
+			}
+
+			uint8_t btnUp = PINB & 0b00001000;
+			if(btnUp == 0 & pulseLength >= MIN_PULSE_LENGTH)
+			{
+				pulseLength-= PULSE_LENGTH_STEP;
+			}	
+	}	
 }
 
-//Изменяет уровень сигнала на выходе.
+//Change level on output.
 void changeLevelOnOutput()
 {
 	if(pulseLevel == 1)
@@ -48,33 +53,26 @@ void changeLevelOnOutput()
 }
 
 int main(void)
-{
-	TIMSK0 = TIMSK0 | 2;
-	TCCR0A = TCCR0A | 5;
-	DDRB = 1;
-	PORTB = 0x18; //Подключаем кнопки через резисторы к +.
-	sei(); //Разрешаем прерывания.
+{	 
+	TCCR0B = 0b00000100; //Mcu clock/256
+	TIMSK0 = 0b00000010; //Allow interrupt for tc0.	
+
+	DDRB = 1; //Setup output on PB0.
+	PORTB = 0x18; //Internal resistors for button to plus line.
 	
-	pulseLength = 400000; //2 секунды длина импульса.
-	pulseLength = 200000; //0,5 секунды длина импульса или 1Гц. 
-	pulseLength = 20000; //~10гц
-	pulseLength = 2000; //100 гц
+	sei(); //Allow all interupts.
 	
-	changeLevelOnOutput(); //Устанавливаем 1 на выходе.
-	
+	changeLevelOnOutput(); //Setup logic 1.
+	pulseLength = START_PULSE_LENGTH;
     while (1) 
     {
-		pulseLengthCounter++; //Считаем длину импульса.
+		pulseLengthCounter++; //Count pulse length.
 		
 		if(pulseLengthCounter > pulseLength)
 		{
 			pulseLengthCounter = 0;
 			changeLevelOnOutput();
 		}
-		//changeLevelOnOutput();
-		//_delay_ms(50);
-		//changeLevelOnOutput();
-		//_delay_ms(50);
     }
 }
 
